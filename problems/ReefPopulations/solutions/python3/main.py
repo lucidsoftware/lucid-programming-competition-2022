6ca003343548fb99f1695309a11084e17125b854
Tylan McKinney <tmcKinney@lucidchart.com>
2022-09-06 10:15:40 -0600

    Speed up Reef Populations python3 solution, add test running file

--- problems/ReefPopulations/solutions/python3/main.py
+++ problems/ReefPopulations/solutions/python3/main.py
@@ -1,10 +1,13 @@
-n, q = map(int, input().split())
-reef = [0]
-reef.extend(map(int, input().split()))
+from sys import stdin, stdout
+
+n, q = map(int, stdin.readline().split())
+reef = [0] + list(map(int, stdin.readline().split()))
 
 for i in range(2, n+1):
     reef[i] ^= reef[i-1]
 
 for _ in range(q):
-    x, y = map(int, input().split())
-    print(reef[y] ^ reef[x-1])
+    x, y = map(int, stdin.readline().split())
+    stdout.write('{}\n'.format(reef[y] ^ reef[x-1]))
+
+stdout.flush()
