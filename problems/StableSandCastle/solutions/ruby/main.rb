1476e8d930b554567102d69f6750356ca32e6bed
Ella Moskun <emoskun@lucidchart.com>
2022-09-13 15:36:19 -0600

    fixup! Ella solves AtlantisCode and StableSandCastle

--- problems/StableSandCastle/solutions/ruby/main.rb
+++ problems/StableSandCastle/solutions/ruby/main.rb
@@ -1,17 +1,11 @@
 gets
-$wall = [0, 0, 0] + gets.split(' ').map(&:to_i) + [0, 0, 0]
-
-def check(a, b)
-    if $wall[a] - $wall[b] > 4
-        puts("False")
-        exit
-    end
-end
-
-(3..$wall.length - 4).each do |i|
+wall = [0, 0, 0] + gets.split(' ').map(&:to_i) + [0, 0, 0]
+(3..wall.length - 4).each do |i|
     (-3..3).each do |j|
-        check(i, i + j)
+        if wall[i] - wall[i + j] > 4
+            puts("False")
+            exit
+        end
     end
 end
-
 puts("True")
