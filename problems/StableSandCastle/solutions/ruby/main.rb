3cdee7c04d2b8b184eaab75e5302579fde44c3a6
Ella Moskun <emoskun@lucidchart.com>
2022-10-04 09:35:47 -0600

    formatting, please ignore

--- problems/StableSandCastle/solutions/ruby/main.rb
+++ problems/StableSandCastle/solutions/ruby/main.rb
@@ -3,9 +3,9 @@ wall = [0, 0, 0] + gets.split(' ').map(&:to_i) + [0, 0, 0]
 (3..wall.length - 4).each do |i|
     (-3..3).each do |j|
         if wall[i] - wall[i + j] > 4
-            puts("False")
+            puts('False')
             exit
         end
     end
 end
-puts("True")
+puts('True')
